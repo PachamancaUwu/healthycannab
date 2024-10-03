415c976cb0e8c8f61e8ada059cca3bce8eabf9d4
Michael <michaelorihuela00@gmail.com>
2024-10-03 18:25:31 -0500

    Conectividad de las vistas

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace healthycannab.Controllers
+{
+    
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+
+        public ContactoController(ILogger<ContactoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
