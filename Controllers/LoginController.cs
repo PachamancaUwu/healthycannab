8341cb8c30e36eef406cf64facf7a2ad4961d793
Michael <michaelorihuela00@gmail.com>
2024-10-04 00:09:56 -0500

    Funcionalidad Login Ver.1

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using healthycannab.Models;
+using healthycannab.Data;
 
 namespace healthycannab.Controllers
 {
@@ -12,21 +14,40 @@ namespace healthycannab.Controllers
     public class LoginController : Controller
     {
         private readonly ILogger<LoginController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public LoginController(ILogger<LoginController> logger)
+        public LoginController(ILogger<LoginController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Login(Usuario model)
         {
-            return View("Error!");
+            if (ModelState.IsValid)
+            {
+                // Verifica si las credenciales son correctas
+                var user = _context.DataUsuario.FirstOrDefault(u => u.Correo == model.Correo && u.Contrasena == model.Contrasena);
+                if (user != null)
+                {
+                    // Inicio de sesión exitoso
+                    TempData["SuccessMessage"] = "Inicio de sesión exitoso."; // Almacena en TempData
+                    return RedirectToAction("Inicio", "Main");
+                }
+                else
+                {
+                    // Credenciales incorrectas
+                    TempData["ErrorMessage"] = "Correo o contraseña incorrectos."; // Almacena en TempData
+                }
+            }
+            // Si llegamos aquí, algo falló, volvemos a mostrar la vista de inicio de sesión con el modelo
+            return View(model);
         }
     }
 }
