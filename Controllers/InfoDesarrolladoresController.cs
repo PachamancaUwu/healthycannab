b3b801504723bfb4fe6f9b578ce59e49a317f2b3
Michael Orihuela <michaelorihuela00@gmail.com>
2024-10-12 23:04:59 -0500

    Informacion de los desarrolladores

---
+++ Controllers/InfoDesarrolladoresController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace healthycannab.Controllers
+{
+   
+    public class InfoDesarrolladores : Controller
+    {
+        private readonly ILogger<InfoDesarrolladores> _logger;
+
+        public InfoDesarrolladores(ILogger<InfoDesarrolladores> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult infoDesarrolladores()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
